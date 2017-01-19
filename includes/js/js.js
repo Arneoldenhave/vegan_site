a81ae7a358a3f0f2e53ef9a6f82c09439bb5b16a
Arne Oldenhave <arneoldenhave@gmail.com>
2017-01-19 17:00:50 +0100

    table image, opacity, text in middle

--- includes/js/js.js
+++ includes/js/js.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
           $('#photosgrid img').mouseover(function () {
               $(this).animate({
-                  opacity: 0.75,
+                  opacity: 0.60,
                   // left: '+=50',
                   // height: 'toggle'
               }, 1000, function () {
@@ -9,3 +9,21 @@ $(document).ready(function () {
               });
           });
       });
+
+$(document).ready(function () {
+          $('#photosgrid img').mouseleave(function () {
+              $(this).animate({
+                  opacity: 1,
+                  // left: '+=50',
+                  // height: 'toggle'
+              }, 1000, function () {
+                  // Animation complete.
+              });
+          });
+      });
+
+
+
+
+
+
