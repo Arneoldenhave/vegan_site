5144cef0f58e0c0401862925e39d0dfca1e1b0f2
Arne Oldenhave <arneoldenhave@gmail.com>
2017-01-19 17:34:31 +0100

    hard reset

--- includes/js/js.js
+++ includes/js/js.js
@@ -1,29 +1,13 @@
 $(document).ready(function () {
-          $('#photosgrid img').mouseover(function () {
-              $(this).animate({
-                  opacity: 0.60,
-                  // left: '+=50',
-                  // height: 'toggle'
-              }, 1000, function () {
-                  // Animation complete.
-              });
-          });
-      });
+  $('#photosgrid img').mouseover(function () {
+      $(this).animate({
+          opacity: 0.60,
+      }, 1000);
+  });
 
-$(document).ready(function () {
-          $('#photosgrid img').mouseleave(function () {
-              $(this).animate({
-                  opacity: 1,
-                  // left: '+=50',
-                  // height: 'toggle'
-              }, 1000, function () {
-                  // Animation complete.
-              });
-          });
-      });
-
-
-
-
-
-
+  $('#photosgrid img').mouseleave(function () {
+      $(this).animate({
+          opacity: 1,
+      }, 1000);
+  });
+});
